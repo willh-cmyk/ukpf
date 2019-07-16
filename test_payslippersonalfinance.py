d3e8e43cf545e204938511b133f719de11898301
William Holtam <william.holtam@gmail.com>
2019-07-16 08:55:02 +0100

    Extended tests for __init__

--- test_payslippersonalfinance.py
+++ test_payslippersonalfinance.py
@@ -14,6 +14,19 @@ class TestPayslipPersonalFinance(unittest.TestCase):
         self.negative_pspf = PayslipPersonalFinance(negative_salary)
         self.zero_pspf = PayslipPersonalFinance(zero_salary)
     
+    def test_init(self):
+        print("test_init")
+        self.assertEqual(12500, self.pspf.tax_free_allowance)
+        self.assertEqual(0.2, self.pspf.basic_tax_rate)
+        self.assertEqual(0.4, self.pspf.higher_tax_rate)
+        self.assertEqual(0.45, self.pspf.additional_tax_rate)
+        self.assertEqual(50000, self.pspf.upper_tax_threshold)
+#         self.assertEqual(self.pspf.national_insurance_allowance)  # TO DO
+        self.assertEqual(0.12, self.pspf.basic_rate_ni)  # Do it for both contracted in and contracted out employees.
+        self.assertEqual(0.02, self.pspf.higher_rate_ni)
+#         self.assertEqual(, self.upper_ni_threshold)  # Recalculate this for test.
+#         self.assertEqual(self.pspf.sl_repayment_threshold)  # To add
+    
     def test_tax_calculator(self):
         print("test_tax_calculator")
         self.assertEqual(3300, self.positive_pspf.tax_calculator())
