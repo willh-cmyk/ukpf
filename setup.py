7d7501515ecbbb5d939867aa84fc4960784e5d3e
William Holtam <william.holtam@datalytyx.com>
2019-11-30 14:33:16 +0000

    Added additional fields to setup.

--- setup.py
+++ setup.py
@@ -4,18 +4,22 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='ukpf',
-     version='0.1',
-     scripts=['payslip.py'],
+     name="ukpf",
+     version="0.1",
+     scripts=["payslip.py"],
      author="William Holtam",
+     author_email="william.holtam@gmail.com",
      description="A personal finance package for the UK tax system",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/williamholtam/ukpersonalfinance",
      packages=setuptools.find_packages(),
+     keywords=["PERSONAL", "FINANCE", "UK", "PAYSLIP"],
      classifiers=[
+         "Development Status :: 3 - Alpha",
          "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+         "License :: OSI Approved :: "
+         "GNU General Public License v3 or later (GPLv3+)",
          "Operating System :: OS Independent",
      ],
  )
