d59a7ae5ec95462ec66afce98ade10aef16addc0
William Holtam <william.holtam@datalytyx.com>
2019-11-30 14:24:20 +0000

    Refactored code layout to prevent long lines.

--- test_payslip.py
+++ test_payslip.py
@@ -1,4 +1,4 @@
-"""Unittesting for payslippersonalfinance module"""
+"""Unittesting for payslip module"""
 
 import unittest
 from payslip import Payslip
@@ -12,44 +12,53 @@ class TestPayslip(unittest.TestCase):
             'medium': 29000,
             'high': 45000,
             'negative': -300,
-            'string': "test"}
+            'string': "test",
+        }
         self.interest = {
             'principal': 2000,
             'rate': 0.02,
-            'time': 10}
+            'time': 10,
+        }
         self.exp_interest = {
             'simple': 400.0,
-            'compound': 437.98883998951464}
+            'compound': 437.98883998951464,
+        }
         # exp short for expected
         self.exp_tax = {
             'low': 0,
             'medium': 3300,
-            'high': 6500}
+            'high': 6500,
+        }
         self.exp_ni = {
             'low': 44.64,
             'medium': 2444.64,
-            'high': 4364.639999999999}
+            'high': 4364.639999999999,
+        }
         self.exp_sl = {
             'low': 0,
             'medium': 298.08,
-            'high': 1738.08}
+            'high': 1738.08,
+        }
         self.exp_ae_pension = {
             'low': 0,
             'medium': 2320.0,
-            'high': 3600.0}
+            'high': 3600.0,
+        }
         self.exp_comp_interest = {
             'low': 0,
             'medium': 2320.0,
-            'high': 3600.0}
+            'high': 3600.0,
+        }
         self.exp_simple_interest = {
             'low': 0,
             'medium': 2320.0,
-            'high': 3600.0}
+            'high': 3600.0,
+        }
 
     def test_init(self):
         for (key, salary) in self.salary.items():
             if key in self.salary_names:
-                pspf = PayslipPersonalFinance(salary)
+                pspf = Payslip(salary)
                 self.assertEqual(12500, pspf.tax_free_allowance)
                 self.assertEqual(0.2, pspf.basic_tax_rate)
                 self.assertEqual(0.4, pspf.higher_tax_rate)
@@ -65,39 +74,62 @@ class TestPayslip(unittest.TestCase):
                 self.assertEqual(0.03, pspf.ae_pension_employer_contrib)
                 self.assertEqual(0.05, pspf.ae_pension_personal_contrib)
             elif salary == "test":
-                self.assertRaises(TypeError, PayslipPersonalFinance, self.salary['string'])
+                self.assertRaises(TypeError, Payslip, self.salary['string'])
             elif salary == -300:
-                self.assertRaises(ValueError, PayslipPersonalFinance, self.salary['negative'])
+                self.assertRaises(ValueError, Payslip, self.salary['negative'])
 
     def test_tax_calculator(self):
         for salary_name in self.salary_names:
-            pspf = PayslipPersonalFinance(self.salary[salary_name])
+            pspf = Payslip(self.salary[salary_name])
             self.assertEqual(self.exp_tax[salary_name], pspf.tax_calculator())
 
     def test_national_insurance_calculator(self):
         for salary_name in self.salary_names:
-            pspf = PayslipPersonalFinance(self.salary[salary_name])
-            self.assertEqual(self.exp_ni[salary_name], pspf.national_insurance_calculator())
+            pspf = Payslip(self.salary[salary_name])
+            self.assertEqual(
+                self.exp_ni[salary_name],
+                pspf.national_insurance_calculator()
+            )
 
     def test_student_loan_calculator(self):
         for salary_name in self.salary_names:
-            pspf = PayslipPersonalFinance(self.salary[salary_name])
-            self.assertEqual(self.exp_sl[salary_name], pspf.student_loan_repayment_calculator())
+            pspf = Payslip(self.salary[salary_name])
+            self.assertEqual(
+                self.exp_sl[salary_name],
+                pspf.student_loan_repayment_calculator(),
+            )
 
     def test_autoenrolement_pension(self):
         for salary_name in self.salary_names:
-            pspf = PayslipPersonalFinance(self.salary[salary_name])
-            self.assertEqual(self.exp_ae_pension[salary_name], pspf.autoenrolement_pension())
+            pspf = Payslip(self.salary[salary_name])
+            self.assertEqual(
+                self.exp_ae_pension[salary_name],
+                pspf.autoenrolement_pension(),
+            )
 
     def test_compound_interest_calculator(self):
         for salary_name in self.salary_names:
-            pspf = PayslipPersonalFinance(self.salary[salary_name])
-            self.assertEqual(self.exp_interest['compound'], pspf.compound_interest_calculator(self.interest['principal'], self.interest['rate'], self.interest['time']))
+            pspf = Payslip(self.salary[salary_name])
+            self.assertEqual(
+                self.exp_interest['compound'],
+                pspf.compound_interest_calculator(
+                    self.interest['principal'],
+                    self.interest['rate'],
+                    self.interest['time'],
+                )
+            )
 
     def test_simple_interest_calculator(self):
         for salary_name in self.salary_names:
-            pspf = PayslipPersonalFinance(self.salary[salary_name])
-            self.assertEqual(self.exp_interest['simple'], pspf.simple_interest_calculator(self.interest['principal'], self.interest['rate'], self.interest['time']))
+            pspf = Payslip(self.salary[salary_name])
+            self.assertEqual(
+                self.exp_interest['simple'],
+                pspf.simple_interest_calculator(
+                    self.interest['principal'],
+                    self.interest['rate'],
+                    self.interest['time'],
+                )
+            )
 
 
 if __name__ == '__main__':
